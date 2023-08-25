8323aea8c80910cb5fd1da9190bd186d0db3fc1d
Hyungju Kwon <hyungju.kwon@gmail.com>
2023-08-25 17:26:22 -0400

    finl commit

--- features/iceCream/iceCreamSlice.js
+++ features/iceCream/iceCreamSlice.js
@@ -1,4 +1,5 @@
 const { createSlice } = require('@reduxjs/toolkit')
+const { cakeActions } = require('../cake/cakeSlice')
 
 const initialState = {
 	numOfIceCreams: 20,
@@ -14,6 +15,18 @@ const iceCreamSlice = createSlice({
 		restocked: (state, action) => {
 			state.numOfIceCreams += action.payload
 		},
+	},
+	// extraReducers: {
+	// 	// when the shop has a sale in which everyone gets an ice cream when ordering a cake
+	// 	['cake/ordered']: (state) => {
+	// 		state.numOfIceCreams--
+	// 	},
+	// },
+	// same as above
+	extraReducers: (builder) => {
+		builder.addCase(cakeActions.ordered, (state) => {
+			state.numOfIceCreams--
+		})
 	},
 })
 
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
 const iceCreamActions =
 	require('./features/iceCream/iceCreamSlice').iceCreamActions
+const fetchUsers = require('./features/user/userSlice').fetchUsers
 
 console.log('Initial State:', store.getState())
 
 const unsubscribe = store.subscribe(() => {
-	console.log('Updated State:', store.getState())
+	// commented to test logger
+	// console.log('Updated State:', store.getState())
 })
 
 store.dispatch(cakeActions.ordered())
@@ -18,4 +20,6 @@ store.dispatch(iceCreamActions.ordered())
 store.dispatch(iceCreamActions.ordered())
 store.dispatch(iceCreamActions.restocked(2))
 
-unsubscribe()
+store.dispatch(fetchUsers())
+
+// unsubscribe()
